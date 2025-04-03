e6a646b60ec350a6c559ff34f54588d85eb7ec72
Netz <49691921+Netz00@users.noreply.github.com>
2025-04-03 22:33:22 +0200

    Extract About modal from MarkThree

--- src/components/MarkThree.js
+++ src/components/MarkThree.js
@@ -11,13 +11,12 @@ import raw from "raw.macro";
 import _ from "lodash";
 import $ from "jquery";
 import { get, set } from "idb-keyval";
-import me from "../img/me.jpg";
-import coffee from "../img/coffee.png";
 import "typeface-roboto-slab";
 import { SearchModal } from "./modals/Search";
 import { DocsModal } from "./modals/Docs";
 import { SettingsModal } from "./modals/Settings";
 import { ContactModal } from "./modals/Contact";
+import { AboutModal } from "./modals/About";
 const tryItNowText = raw("./tryItNow.md");
 
 class MarkThree extends React.Component {
@@ -504,64 +503,7 @@ class MarkThree extends React.Component {
         )}
 
         {this.state.showAbout && (
-          <div className="m2-about modal is-active">
-            <div
-              className="modal-background"
-              onClick={() => this.setState({ showAbout: false })}
-            ></div>
-            <div className="modal-card">
-              <header className="modal-card-head">
-                <p className="modal-card-title">About</p>
-                <button
-                  className="delete"
-                  aria-label="close"
-                  onClick={() => this.setState({ showAbout: false })}
-                ></button>
-              </header>
-              <section className="modal-card-body">
-                <p>
-                  MarkThree was created by me, Anthony Garvan. I&apos;m a
-                  software developer based out of Chicago. I love spending time
-                  with my family, working with my team, and tinkering with
-                  random projects like this one.
-                </p>
-
-                <p>
-                  MarkThree is my second attempt at a markdown editor, and
-                  obviously my best. It took many months to get right, if you
-                  enjoy using it please consider showing your appreciation by
-                  buying me a cup of coffee ☕❤️.
-                </p>
-                <div className="m2-me">
-                  <img className="m2-profile" src={me} alt="developer" />
-                  <div></div>
-                </div>
-              </section>
-              <footer className="modal-card-foot">
-                <a href="/privacy.txt" target="_blank">
-                  Privacy
-                </a>
-                <a href="/terms.txt" target="_blank">
-                  Terms
-                </a>
-                <a
-                  href="https://github.com/anthonygarvan/markthree"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Source
-                </a>
-                <a
-                  className="m2-coffee is-pulled-right"
-                  href="https://www.buymeacoffee.com/GDsZofV"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={coffee} alt="Buy Me A Coffee" />
-                </a>
-              </footer>
-            </div>
-          </div>
+          <AboutModal closeAbout={() => this.setState({ showAbout: false })} />
         )}
 
         {this.state.showHelp && (
