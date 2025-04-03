a7308574094103731df67750d5aae3162049ae31
Netz <49691921+Netz00@users.noreply.github.com>
2025-04-03 22:28:56 +0200

    Extract Docs modal from MarkThree

--- src/components/MarkThree.js
+++ src/components/MarkThree.js
@@ -1,5 +1,4 @@
 import React from "react";
-import moment from "moment";
 import Shelf from "./Shelf";
 import shortid from "shortid";
 import Doc from "./Doc";
@@ -18,6 +17,7 @@ import coffee from "../img/coffee.png";
 import { faBolt, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import "typeface-roboto-slab";
 import { SearchModal } from "./modals/Search";
+import { DocsModal } from "./modals/Docs";
 const tryItNowText = raw("./tryItNow.md");
 
 class MarkThree extends React.Component {
@@ -466,114 +466,20 @@ class MarkThree extends React.Component {
         )}
 
         {this.state.showDocs && (
-          <div className="m2-docs modal is-active">
-            <div
-              className="modal-background"
-              onClick={() => this.setState({ showDocs: false })}
-            ></div>
-            <div className="modal-card">
-              <header className="modal-card-head">
-                <p className="modal-card-title">Docs</p>
-                <button
-                  className="delete"
-                  aria-label="close"
-                  onClick={() => this.setState({ showDocs: false })}
-                ></button>
-              </header>
-              <section className="modal-card-body">
-                <div>
-                  <label className="m2-import">
-                    <span
-                      className="button is-text is-clear"
-                      disabled={this.state.offlineMode}
-                    >
-                      Import
-                    </span>
-                    <input
-                      type="file"
-                      onChange={this.handleImport}
-                      accept=".txt,.md"
-                      disabled={this.state.offlineMode}
-                    />
-                  </label>
-                  <button
-                    className="button is-outline"
-                    onClick={this.startNewFile}
-                    disabled={this.state.offlineMode}
-                  >
-                    New
-                  </button>
-                </div>
-                <table className="table is-striped is-fullwidth">
-                  <thead>
-                    <tr>
-                      <th></th>
-                      <th>
-                        <abbr title="Filename">Filename</abbr>
-                      </th>
-                      <th>
-                        <abbr title="Last modified">Last modified</abbr>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {this.state.docs &&
-                      this.state.docs
-                        .filter((f) => !!f.archived === this.state.viewArchive)
-                        .map((f) => (
-                          <tr key={f.id}>
-                            <td>
-                              <div className="select">
-                                <select
-                                  value={"default"}
-                                  onChange={(e) => this.takeFileAction(e, f)}
-                                >
-                                  <option hidden value="default"></option>
-                                  <option value="rename">Rename</option>
-                                  {f.id === this.state.currentDoc && (
-                                    <option value="export">Export</option>
-                                  )}
-                                  <option value="toggleArchive">
-                                    {!f.archived ? "Archive" : "Move to docs"}
-                                  </option>
-                                  <option value="delete">Delete</option>
-                                </select>
-                              </div>
-                            </td>
-                            <td
-                              className={
-                                f.id === this.state.currentDoc
-                                  ? "m2-is-current-doc"
-                                  : undefined
-                              }
-                            >
-                              <a onClick={() => this.openFile(f.id)}>
-                                {f.title ? (
-                                  <abbr title={f.title}>
-                                    {f.title.substring(0, 20)}
-                                  </abbr>
-                                ) : (
-                                  "Untitled"
-                                )}
-                              </a>
-                            </td>
-                            <td>{moment(f.lastModified).fromNow()}</td>
-                          </tr>
-                        ))}
-                  </tbody>
-                </table>
-                <div className="m2-footer">
-                  <a
-                    onClick={() =>
-                      this.setState({ viewArchive: !this.state.viewArchive })
-                    }
-                  >
-                    {this.state.viewArchive ? "View docs" : "View archive"}
-                  </a>
-                </div>
-              </section>
-            </div>
-          </div>
+          <DocsModal
+            closeDocsModal={() => this.setState({ showDocs: false })}
+            handleImport={this.handleImport}
+            startNewFile={this.startNewFile}
+            offlineMode={this.state.offlineMode}
+            docs={this.state.docs}
+            viewArchive={this.state.viewArchive}
+            toggleViewArchive={() =>
+              this.setState({ viewArchive: !this.state.viewArchive })
+            }
+            currentDoc={this.state.currentDoc}
+            openFile={this.openFile}
+            takeFileAction={this.takeFileAction}
+          />
         )}
 
         {this.state.showSettings && (
---
+++ src/components/modals/Docs.js
@@ -0,0 +1,114 @@
+import React from "react";
+import moment from "moment";
+
+export const DocsModal = ({
+  closeDocsModal,
+  handleImport,
+  startNewFile,
+  offlineMode,
+  docs,
+  viewArchive,
+  toggleViewArchive,
+  currentDoc,
+  openFile,
+  takeFileAction,
+}) => {
+  return (
+    <div className="m2-docs modal is-active">
+      <div className="modal-background" onClick={closeDocsModal}></div>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Docs</p>
+          <button
+            className="delete"
+            aria-label="close"
+            onClick={closeDocsModal}
+          ></button>
+        </header>
+        <section className="modal-card-body">
+          <div>
+            <label className="m2-import">
+              <span className="button is-text is-clear" disabled={offlineMode}>
+                Import
+              </span>
+              <input
+                type="file"
+                onChange={handleImport}
+                accept=".txt,.md"
+                disabled={offlineMode}
+              />
+            </label>
+            <button
+              className="button is-outline"
+              onClick={startNewFile}
+              disabled={offlineMode}
+            >
+              New
+            </button>
+          </div>
+          <table className="table is-striped is-fullwidth">
+            <thead>
+              <tr>
+                <th></th>
+                <th>
+                  <abbr title="Filename">Filename</abbr>
+                </th>
+                <th>
+                  <abbr title="Last modified">Last modified</abbr>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {docs &&
+                docs
+                  .filter((f) => !!f.archived === viewArchive)
+                  .map((f) => (
+                    <tr key={f.id}>
+                      <td>
+                        <div className="select">
+                          <select
+                            value={"default"}
+                            onChange={(e) => takeFileAction(e, f)}
+                          >
+                            <option hidden value="default"></option>
+                            <option value="rename">Rename</option>
+                            {f.id === currentDoc && (
+                              <option value="export">Export</option>
+                            )}
+                            <option value="toggleArchive">
+                              {!f.archived ? "Archive" : "Move to docs"}
+                            </option>
+                            <option value="delete">Delete</option>
+                          </select>
+                        </div>
+                      </td>
+                      <td
+                        className={
+                          f.id === currentDoc ? "m2-is-current-doc" : undefined
+                        }
+                      >
+                        <a onClick={() => openFile(f.id)}>
+                          {f.title ? (
+                            <abbr title={f.title}>
+                              {f.title.substring(0, 20)}
+                            </abbr>
+                          ) : (
+                            "Untitled"
+                          )}
+                        </a>
+                      </td>
+                      <td>{moment(f.lastModified).fromNow()}</td>
+                    </tr>
+                  ))}
+            </tbody>
+          </table>
+          <div className="m2-footer">
+            <a onClick={toggleViewArchive}>
+              {viewArchive ? "View docs" : "View archive"}
+            </a>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
