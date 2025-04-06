8984e24ce9fe293ef917eb63defef3a45a96f440
Netz <49691921+Netz00@users.noreply.github.com>
2025-04-06 12:13:52 +0200

    Refactor Shelf component

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTimes,
@@ -12,107 +12,121 @@ import anonymous from "../img/anonymous.png";
 import user from "../img/user.png";
 import { set, get } from "idb-keyval";
 
-class Shelf extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getUserProfile = this.getUserProfile.bind(this);
-  }
+const Shelf = (props) => {
+  const [userProfile, setUserProfile] = useState({
+    userEmail: "",
+    photoUrl: "",
+    userName: "",
+  });
 
-  componentDidMount() {
-    this.getUserProfile();
-  }
-
-  getUserProfile() {
-    if (this.props.tryItNow) {
-      this.setState({
-        userEmail: "anonymous.bunny@gmail.com",
-        photoUrl: anonymous,
-        userName: "Anonymous Bunny",
-      });
-    } else {
-      if (this.props.gapi) {
-        const profile = this.props.gapi.auth2
-          .getAuthInstance()
-          .currentUser.get()
-          .getBasicProfile();
-        const userEmail = profile.getEmail();
-        const userName = profile.getName();
-        this.setState({ userEmail, photoUrl: profile.getImageUrl(), userName });
-        set("userEmail", userEmail);
-        set("userName", userName);
-      } else {
-        get("userEmail").then((userEmail) => {
-          get("userName").then((userName) => {
-            this.setState({ userEmail, userName, photoUrl: user });
-          });
+  const getUserProfile = useCallback(async () => {
+    try {
+      if (props.tryItNow) {
+        setUserProfile({
+          userEmail: "anonymous.bunny@gmail.com",
+          photoUrl: anonymous,
+          userName: "Anonymous Bunny",
         });
-      }
-    }
-  }
+      } else {
+        if (props.gapi) {
+          const profile = props.gapi.auth2
+            .getAuthInstance()
+            .currentUser.get()
+            .getBasicProfile();
+          const userEmail = profile.getEmail();
+          const userName = profile.getName();
 
-  render() {
-    return this.props.showShelf ? (
-      <div className="m2-shelf">
-        <div className="m2-profile">
-          <div className="m2-profile-photo">
-            <img src={this.state.photoUrl} alt="profile" />
-          </div>
-          <div className="m2-username">{this.state.userName}</div>
-          <div className="m2-email">{this.state.userEmail}</div>
-          <div className="m2-switch-user">
-            <button
-              className="button is-clear"
-              onClick={this.props.handleSwitchUser}
-              disabled={this.props.offlineMode}
-            >
-              Switch user
-            </button>
-            <button
-              className="button is-clear"
-              onClick={this.props.handleLogout}
-              disabled={this.props.offlineMode}
-            >
-              Sign out
-            </button>
-          </div>
+          setUserProfile({
+            userEmail,
+            photoUrl: profile.getImageUrl(),
+            userName,
+          });
+
+          await set("userEmail", userEmail);
+          await set("userName", userName);
+        } else {
+          const userEmail = await get("userEmail");
+          const userName = await get("userName");
+
+          setUserProfile({
+            userEmail,
+            userName,
+            photoUrl: user,
+          });
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching user profile:", error);
+    }
+  }, [props.gapi, props.tryItNow]);
+
+  useEffect(() => {
+    getUserProfile();
+  }, [getUserProfile]);
+
+  const { userEmail, photoUrl, userName } = userProfile;
+
+  return props.showShelf ? (
+    <div className="m2-shelf">
+      <div className="m2-profile">
+        <div className="m2-profile-photo">
+          <img src={photoUrl} alt="profile" />
         </div>
-        <div className="m2-menu-links">
-          <div>
-            <a onClick={this.props.showSearch}>
-              <FontAwesomeIcon icon={faSearch} />
-              &nbsp;&nbsp;Search
-            </a>
-          </div>
-          <div>
-            <a onClick={() => this.props.showDocs(true)}>
-              <FontAwesomeIcon icon={faFileAlt} />
-              &nbsp;&nbsp;Docs
-            </a>
-          </div>
-          <div>
-            <a onClick={this.props.showSettings}>
-              <FontAwesomeIcon icon={faCog} />
-              &nbsp;&nbsp;Settings
-            </a>
-          </div>
+        <div className="m2-username">{userName}</div>
+        <div className="m2-email">{userEmail}</div>
+        <div className="m2-switch-user">
+          <button
+            className="button is-clear"
+            onClick={props.handleSwitchUser}
+            disabled={props.offlineMode}
+          >
+            Switch user
+          </button>
+          <button
+            className="button is-clear"
+            onClick={props.handleLogout}
+            disabled={props.offlineMode}
+          >
+            Sign out
+          </button>
         </div>
-        <div className="m2-menu-footer">
-          <a onClick={this.props.showContact}>Contact</a>
-          <a onClick={this.props.showHelp}>Help</a>
-          <a onClick={this.props.showAbout}>About</a>
-        </div>
-        <a className="m2-close" onClick={() => this.props.setShelf(false)}>
-          <FontAwesomeIcon icon={faTimes} />
-        </a>
       </div>
-    ) : (
-      <div className="m2-menu">
-        <a className="m2-ellipsis" onClick={() => this.props.setShelf(true)}>
-          <FontAwesomeIcon icon={faEllipsisV} />
-        </a>
+      <div className="m2-menu-links">
+        <div>
+          <a onClick={props.showSearch}>
+            <FontAwesomeIcon icon={faSearch} />
+            &nbsp;&nbsp;Search
+          </a>
+        </div>
+        <div>
+          <a onClick={() => props.showDocs(true)}>
+            <FontAwesomeIcon icon={faFileAlt} />
+            &nbsp;&nbsp;Docs
+          </a>
+        </div>
+        <div>
+          <a onClick={props.showSettings}>
+            <FontAwesomeIcon icon={faCog} />
+            &nbsp;&nbsp;Settings
+          </a>
+        </div>
       </div>
-    );
-  }
-}
+      <div className="m2-menu-footer">
+        <a onClick={props.showContact}>Contact</a>
+        <a onClick={props.showHelp}>Help</a>
+        <a onClick={props.showAbout}>About</a>
+      </div>
+      <a className="m2-close" onClick={() => props.setShelf(false)}>
+        <FontAwesomeIcon icon={faTimes} />
+      </a>
+    </div>
+  ) : (
+    <div className="m2-menu">
+      <a className="m2-ellipsis" onClick={() => props.setShelf(true)}>
+        <FontAwesomeIcon icon={faEllipsisV} />
+      </a>
+    </div>
+  );
+};
 
 export default Shelf;
